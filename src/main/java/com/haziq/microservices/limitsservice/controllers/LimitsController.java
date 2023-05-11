86155eaf5e6761e1b5910758bd1c1cbc56399f6c
Muhammad Haziq <beinghaziq@gmail.com>
2023-05-11 16:28:31 +0500

    limits attributes moved to application.properties

--- src/main/java/com/haziq/microservices/limitsservice/controllers/LimitsController.java
+++ src/main/java/com/haziq/microservices/limitsservice/controllers/LimitsController.java
@@ -1,14 +1,17 @@
 package com.haziq.microservices.limitsservice.controllers;
 
+import com.haziq.microservices.limitsservice.configuration.Configuration;
 import com.haziq.microservices.limitsservice.models.Limit;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LimitsController {
-
+  @Autowired
+  private Configuration configuration;
   @GetMapping("/limits")
   public Limit retrieveAllLimits() {
-    return new Limit(1, 2000);
+    return new Limit(configuration.getMinimum(), configuration.getMaximum());
   }
 }
